59313e81a2f1445e4dde0ff51bd8f73f44cbf8a6
dmitochek <dmitryvakh@gmail.com>
2023-08-21 02:30:56 +0300

    Implement nav drawer

--- app/src/main/java/com/example/freekino/presentation/MainActivity.kt
+++ app/src/main/java/com/example/freekino/presentation/MainActivity.kt
@@ -6,7 +6,9 @@ import android.view.MenuItem
 import android.widget.AbsListView
 import android.widget.GridView
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.freekino.R
 import com.example.freekino.data.repository.FilmsRepositoryImpl
 import com.example.freekino.domain.models.VideoBasicInfo
@@ -21,23 +23,49 @@ class MainActivity : AppCompatActivity() {
     private val refreshCatalogUseCase = RefreshCatalogUseCase()
     private lateinit var gridView: GridView
 
+    //menu drawer
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         showFilms(showBasicVideoInfoUseCase.execute())
+
+        drawerLayout = findViewById(R.id.my_drawer_layout)
+        actionBarDrawerToggle = ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close)
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main_menu, menu)
         return true
     }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         //TODO make search and refresh
-        when (item.itemId){
-            R.id.search_button -> Toast.makeText(this,"Search",Toast.LENGTH_SHORT).show()
-            R.id.refresh_button -> Toast.makeText(this,"Refresh",Toast.LENGTH_SHORT).show()
+        if (actionBarDrawerToggle.onOptionsItemSelected(item))
+        {
+            when (item.itemId){
+                R.id.new_movies -> showFilms(showBasicVideoInfoUseCase.execute())
+                R.id.films -> showFilms(showBasicVideoInfoUseCase.execute())
+                R.id.serials -> showFilms(showBasicVideoInfoUseCase.execute())
+                R.id.cartoons -> showFilms(showBasicVideoInfoUseCase.execute())
+                R.id.animated_series -> showFilms(showBasicVideoInfoUseCase.execute())
+            }
+            return true
         }
-        return super.onOptionsItemSelected(item)
+        else {
+            when (item.itemId) {
+                R.id.search_button -> Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show()
+                R.id.refresh_button -> Toast.makeText(this, "Refresh", Toast.LENGTH_SHORT).show()
+            }
+            return super.onOptionsItemSelected(item)
+        }
     }
     private fun showFilms(videos: Array<VideoBasicInfo?>){
         //val clmns = 3
